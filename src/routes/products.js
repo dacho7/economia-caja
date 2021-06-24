3b8311616cff5d50caee0692768993ac4fee3d5d
dacho7 <davidtobarst@gmail.com>
2021-06-23 23:06:21 -0500

    Se agregaron funciones de update Product

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 const router = Router()
-import { createProduct } from '../controllers/products'
+import { createProduct, 
+    findByDescription, 
+    showAllProducts, 
+    updateProductCode, 
+    updateProductDescription 
+} from '../controllers/products'
 
 router.post('/product', createProduct)
+router.get('/products', showAllProducts)
+router.put('/productupdatedescription', updateProductDescription)
+router.put('/productupdatecode', updateProductCode)
+router.get('/product', findByDescription)
 
 export default router;
