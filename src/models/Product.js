dbca197d6dd65cdc2e699d16454f0bde73f18c08
dacho7 <davidtobarst@gmail.com>
2021-09-13 23:44:07 -0500

    add comment in products about state ...

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,50 +1,55 @@
-import { sequelize } from '../database/database';
+import { sequelize } from "../database/database";
 import { DataTypes } from "sequelize";
 
-const Product = sequelize.define('products',{
-    idProduct:{
-        type: DataTypes.BIGINT,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+const Product = sequelize.define(
+  "products",
+  {
+    idProduct: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     code: {
-        type: DataTypes.TEXT,
-        unique: true
+      type: DataTypes.TEXT,
+      unique: true,
     },
     description: {
-        allowNull: false,
-        type: DataTypes.TEXT
+      allowNull: false,
+      type: DataTypes.TEXT,
     },
     costPrice: {
-        allowNull: false,
-        type: DataTypes.REAL
+      allowNull: false,
+      type: DataTypes.REAL,
     },
     salePrice: {
-        allowNull: false,
-        type: DataTypes.REAL
+      allowNull: false,
+      type: DataTypes.REAL,
     },
     quantity: {
-        allowNull: false,
-        type: DataTypes.INTEGER
+      allowNull: false,
+      type: DataTypes.INTEGER,
     },
     expireDate: {
-        type: DataTypes.DATE
+      type: DataTypes.DATE,
     },
     dateUpdate: {
-        type: DataTypes.DATE
+      type: DataTypes.DATE,
     },
     type: {
-        type: DataTypes.TEXT,
-        allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
-    state:{
-        type: DataTypes.TEXT,
-        defaultValue: 'ACTIVE'
-    }
-},{
-    timestamps: false
-});
+    state: {
+      type: DataTypes.TEXT,
+      defaultValue: "ACTIVE",
+      //ACTIVE, AVAILABLE, UNAVAILABLE, WITHOUT-REVIEW
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 // Product.hasMany(Sale, {foreignKey: 'idSale', sourceKey: 'id'})
 // Sale.belongsTo(Product, {foreignKey: 'idSale', sourceKey: 'id'})
